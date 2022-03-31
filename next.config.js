1fc2d176902f11156305bb80c4f14b208fff319f
h2 <ht@live.se>
2022-03-31 23:33:45 +0800

    fix export  custom routes

--- next.config.js
+++ next.config.js
@@ -2,15 +2,6 @@
 module.exports = {
   reactStrictMode: true,
   trailingSlash: false,
-  async redirects() {
-    return [
-      {
-        source: '/',
-        destination: '/front',
-        permanent: false,
-      },
-    ]
-  },
   webpack: (config) => {
     config.module.rules.push({
       test: /\.md$/,
