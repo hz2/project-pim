e944071bf9e640629421984f2b393df3c250dee3
h2 <ht@live.se>
2022-05-15 23:57:59 +0800

    fix msg

--- src/reducer/reducer.ts
+++ src/reducer/reducer.ts
@@ -2,12 +2,15 @@
 import * as React from 'react';
 
 interface IState {
-    msgStatus: boolean
-    msgText: string | undefined
+    msg: {
+        state: boolean
+        text: string | undefined
+        type: 'success' | 'info' | 'warning' | 'error'
+    }
 }
 
 type ACTIONTYPE =
-    | { type: string, data?: string }
+    | { type: string, data?: string | IState }
     | null | undefined
 
 export type ContextType = {
@@ -19,26 +22,31 @@ export type ContextType = {
 export const defaultDispatch: React.Dispatch<ACTIONTYPE> = () => null
 
 export const initialState: IState = {
-    msgStatus: false,
-    msgText: '',
+    msg: {
+        state: false,
+        text: '',
+        type: 'success'
+    }
 }
 
 export const reducer: React.Reducer<IState, ACTIONTYPE> = (state, action) => {
     if (!action) {
         return state
     }
+    const payload = action.data;
     switch (action.type) {
         case "open_msg":
             return {
                 ...state,
-                msgStatus: true,
-                msgText: action.data
+                msg: {
+                    ...initialState.msg,
+                    ...payload?.msg
+                }
             }
         case "close_msg":
             return {
                 ...state,
-                msgStatus: false,
-                msgText: ''
+                msg: initialState.msg
             }
 
         default:
