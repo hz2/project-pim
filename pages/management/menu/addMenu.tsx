5ecf183d76ec8412a72847848b2a999aab1ae86b
h2 <ht@live.se>
2022-06-07 23:57:14 +0800

    add menu tree

--- pages/management/menu/addMenu.tsx
+++ pages/management/menu/addMenu.tsx
@@ -1,13 +1,116 @@
 import * as React from 'react';
 import Box from '@mui/material/Box';
-import Paper from '@mui/material/Paper';
-import Button from '@mui/material/Button';
-import Typography from '@mui/material/Typography';
+import { Paper } from '@mui/material';
+import { TreeView, TreeItem } from '@mui/lab';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import ChevronRightIcon from '@mui/icons-material/ChevronRight';
 import { Checkbox, FormControlLabel, Grid, TextField } from '@mui/material';
 import { formToObj } from '@/utils/utils';
-import { _req } from '@/utils/service';
+import { _get, _req } from '@/utils/service';
+import { UserContext } from "@/components/PageProvider"
+import { IMenu } from "@/types/types";
 
-const AddMenu = React.forwardRef((props, ref) => {
+import { alpha, styled } from '@mui/material/styles';
+import { TreeItemProps, treeItemClasses } from '@mui/lab/TreeItem';
+import Collapse from '@mui/material/Collapse';
+import SvgIcon, { SvgIconProps } from '@mui/material/SvgIcon';
+import { useSpring, animated } from 'react-spring';
+import { TransitionProps } from '@mui/material/transitions';
+
+function MinusSquare(props: SvgIconProps) {
+    return (
+        <SvgIcon fontSize="inherit" style={{ width: 14, height: 14 }} {...props}>
+            {/* tslint:disable-next-line: max-line-length */}
+            <path d="M22.047 22.074v0 0-20.147 0h-20.12v0 20.147 0h20.12zM22.047 24h-20.12q-.803 0-1.365-.562t-.562-1.365v-20.147q0-.776.562-1.351t1.365-.575h20.147q.776 0 1.351.575t.575 1.351v20.147q0 .803-.575 1.365t-1.378.562v0zM17.873 11.023h-11.826q-.375 0-.669.281t-.294.682v0q0 .401.294 .682t.669.281h11.826q.375 0 .669-.281t.294-.682v0q0-.401-.294-.682t-.669-.281z" />
+        </SvgIcon>
+    );
+}
+
+function PlusSquare(props: SvgIconProps) {
+    return (
+        <SvgIcon fontSize="inherit" style={{ width: 14, height: 14 }} {...props}>
+            {/* tslint:disable-next-line: max-line-length */}
+            <path d="M22.047 22.074v0 0-20.147 0h-20.12v0 20.147 0h20.12zM22.047 24h-20.12q-.803 0-1.365-.562t-.562-1.365v-20.147q0-.776.562-1.351t1.365-.575h20.147q.776 0 1.351.575t.575 1.351v20.147q0 .803-.575 1.365t-1.378.562v0zM17.873 12.977h-4.923v4.896q0 .401-.281.682t-.682.281v0q-.375 0-.669-.281t-.294-.682v-4.896h-4.923q-.401 0-.682-.294t-.281-.669v0q0-.401.281-.682t.682-.281h4.923v-4.896q0-.401.294-.682t.669-.281v0q.401 0 .682.281t.281.682v4.896h4.923q.401 0 .682.281t.281.682v0q0 .375-.281.669t-.682.294z" />
+        </SvgIcon>
+    );
+}
+
+function CloseSquare(props: SvgIconProps) {
+    return (
+        <SvgIcon
+            className="close"
+            fontSize="inherit"
+            style={{ width: 14, height: 14 }}
+            {...props}
+        >
+            {/* tslint:disable-next-line: max-line-length */}
+            <path d="M17.485 17.512q-.281.281-.682.281t-.696-.268l-4.12-4.147-4.12 4.147q-.294.268-.696.268t-.682-.281-.281-.682.294-.669l4.12-4.147-4.12-4.147q-.294-.268-.294-.669t.281-.682.682-.281.696 .268l4.12 4.147 4.12-4.147q.294-.268.696-.268t.682.281 .281.669-.294.682l-4.12 4.147 4.12 4.147q.294.268 .294.669t-.281.682zM22.047 22.074v0 0-20.147 0h-20.12v0 20.147 0h20.12zM22.047 24h-20.12q-.803 0-1.365-.562t-.562-1.365v-20.147q0-.776.562-1.351t1.365-.575h20.147q.776 0 1.351.575t.575 1.351v20.147q0 .803-.575 1.365t-1.378.562v0z" />
+        </SvgIcon>
+    );
+}
+
+function TransitionComponent(props: TransitionProps) {
+    const style = useSpring({
+        from: {
+            opacity: 0,
+            transform: 'translate3d(20px,0,0)',
+        },
+        to: {
+            opacity: props.in ? 1 : 0,
+            transform: `translate3d(${props.in ? 0 : 20}px,0,0)`,
+        },
+    });
+
+    return (
+        <animated.div style={style}>
+            <Collapse {...props} />
+        </animated.div>
+    );
+}
+
+
+
+
+const StyledTreeItem = styled((props: TreeItemProps) => (
+    <TreeItem {...props} TransitionComponent={TransitionComponent} />
+))(({ theme }) => ({
+    [`& .${treeItemClasses.iconContainer}`]: {
+        '& .close': {
+            opacity: 0.3,
+        },
+    },
+    [`& .${treeItemClasses.group}`]: {
+        marginLeft: 15,
+        paddingLeft: 18,
+        borderLeft: `1px dashed ${alpha(theme.palette.text.primary, 0.4)}`,
+    },
+}));
+
+type FormRef = {
+    formSubmit: () => void
+}
+interface IPageProps {
+    row: IMenu
+    ref: React.ForwardedRef<FormRef>
+    onSuccess: () => void
+}
+
+
+const AddMenu: React.FC<IPageProps> = React.forwardRef((props, ref) => {
+
+    const [list, setList] = React.useState<IMenu[]>([])
+    const { dispatch } = React.useContext(UserContext)
+
+    const getList = () => {
+        _get('/sys/menu').then(res => {
+            setList(res)
+        })
+    }
+
+    React.useEffect(() => {
+        getList()
+    }, [])
+
     const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
         event.preventDefault();
         const form = new FormData(event.currentTarget);
@@ -30,6 +133,19 @@ const AddMenu = React.forwardRef((props, ref) => {
             }
         }
     }));
+
+    const chooseTreeItem = e=> {
+        console.log('e',e );
+        
+    }
+
+
+    const genTree = (list: IMenu[]) => {
+        const tree = (list: IMenu[]) => list.map(x => <StyledTreeItem nodeId={String(x.id)} label={x.text} key={x.id} onClick={chooseTreeItem}>
+            {Array.isArray(x.children) ? tree(x.children) : null}
+        </StyledTreeItem>)
+        return tree(list)
+    }
 
     return (
         <Paper variant="outlined" sx={{ my: { xs: 3, md: 6 }, p: { xs: 2, md: 3 } }}>
@@ -55,6 +171,19 @@ const AddMenu = React.forwardRef((props, ref) => {
                             fullWidth
                             variant="standard"
                         />
+                    </Grid>
+                    <Grid item xs={12} sm={6}>
+                        <TreeView
+                            aria-label="customized"
+                            defaultExpanded={['1']}
+                            defaultCollapseIcon={<MinusSquare />}
+                            defaultExpandIcon={<PlusSquare />}
+                            defaultEndIcon={<CloseSquare />}
+                            sx={{ height: 264, flexGrow: 1, maxWidth: 400, overflowY: 'auto' }}
+                        >
+                            {genTree(list)}
+                        </TreeView>
+
                     </Grid>
                     <Grid item xs={12} sm={6}>
                         <TextField
