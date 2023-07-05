16a6c85876f5eb33895ec465cd8f770a9890d04d
hz <ht@live.se>
2023-07-05 18:13:28 +0800

    update style

--- src/components/NewHome.tsx
+++ src/components/NewHome.tsx
@@ -1,20 +1,12 @@
-import { Box, Container, Link, Typography } from "@mui/material";
+import { Box, Container, Link, SvgIcon, Typography, createSvgIcon } from "@mui/material";
 import Footer from "./Footer";
 import GitHubIcon from '@mui/icons-material/GitHub';
 import TelegramIcon from '@mui/icons-material/Telegram';
 import Public from '@mui/icons-material/Public';
 import Share from '@mui/icons-material/Share';
 import { Stack } from '@mui/material';
-
 import styles from './NewHome.module.css';
-
-const footerItem = {
-    display: "inline-block",
-    width: "80px",
-    height: "80px",
-    bgcolor: "rgb(242, 242, 242)"
-}
-
+import { footerItemList } from "./NewHomeIcons";
 
 const Linkmap = [
     { name: 'GitHub', icon: GitHubIcon },
@@ -29,7 +21,7 @@ const NewHome = () => {
                 <h2>Get Connected</h2>
                 <Typography >Don&apos;t just stand on the sidelines—be part of 0xc8 ‘s vision. Collaborate, brainstorm, or simply exchange ideas. Let’s unleash our creative potential together.</Typography>
                 {/* <Typography sx={{ }}>Contact</Typography> */}
-                <Stack sx={{pt:2}} direction="row" alignItems="center" justifyContent="center">
+                <Stack sx={{ pt: 2 }} direction="row" alignItems="center" justifyContent="center">
                     {
                         Linkmap.map(x =>
                             <Link
@@ -47,32 +39,14 @@ const NewHome = () => {
                     }  </Stack>
             </Container>
             <Container sx={{ display: 'grid', placeItems: "center", px: "20px", py: "40px" }}>
-                <Box sx={{ display: "flex" }}>
-                    <Box sx={footerItem}>
-                        <svg viewBox="0 0 200 200" fill="none" >
-                            <path d="M200 100L0 0V200L200 100Z" fill="rgb(98, 95, 99)"></path>
-                        </svg>
-                    </Box>
-                    <Box sx={footerItem}>
-                        <svg viewBox="0 0 200 200" fill="none" >
-                            <path fillRule="evenodd" clipRule="evenodd" d="M150 100C177.614 100 200 77.6142 200 50C200 22.3858 177.614 0 150 0C122.386 0 100 22.3858 100 50C100 77.6142 122.386 100 150 100ZM50 200C77.6142 200 100 177.614 100 150C100 122.386 77.6142 100 50 100C22.3858 100 0 122.386 0 150C0 177.614 22.3858 200 50 200Z" fill="rgb(114, 114, 126)"></path>
-                            <path fillRule="evenodd" clipRule="evenodd" d="M50 100C77.6142 100 100 77.6142 100 50C100 22.3858 77.6142 0 50 0C22.3858 0 0 22.3858 0 50C0 77.6142 22.3858 100 50 100ZM150 200C177.614 200 200 177.614 200 150C200 122.386 177.614 100 150 100C122.386 100 100 122.386 100 150C100 177.614 122.386 200 150 200Z" fill="rgb(98, 95, 99)"></path>
-                        </svg>
-                    </Box>
-                </Box>
-                <Box sx={{ display: "flex" }}>
-                    <Box sx={footerItem}>
-                        <svg viewBox="0 0 200 200" fill="none" >
-                            <rect width="100" height="100" fill="rgb(98, 95, 99)"></rect>
-                            <rect x="100" y="100" width="100" height="100" fill="rgb(114, 114, 126)"></rect>
-                        </svg>
-                    </Box>
-                    <Box sx={footerItem}>
-                        <svg viewBox="0 0 200 200" fill="none" >
-                            <path fillRule="evenodd" clipRule="evenodd" d="M200 200V0C89.543 0 0 89.5431 0 200H200Z" fill="rgb(98, 95, 99)"></path>
-                        </svg>
-                    </Box>
-                </Box>
+                <Box sx={{ display: "flex", flexWrap: "wrap", width: "160px" }}>
+                    {
+                        footerItemList.map((El, i) => <SvgIcon key={i + '_footeritem'} sx={{
+                            width: 1 / 2,
+                            height: 1 / 2,
+                            bgcolor: "rgb(242, 242, 242)"
+                        }}>{El}</SvgIcon>)
+                    } </Box>
                 <Footer
                     title="Footer"
                     description="Something here to give the footer a purpose!"
---
+++ src/components/NewHomeIcons.tsx
@@ -0,0 +1,18 @@
+
+export const footerItemList = [
+     <svg viewBox="0 0 200 200" fill="none" >
+        <path d="M200 100L0 0V200L200 100Z" fill="rgb(98, 95, 99)"></path>
+    </svg>,
+    <svg viewBox="0 0 200 200" fill="none" >
+        <path fillRule="evenodd" clipRule="evenodd" d="M150 100C177.614 100 200 77.6142 200 50C200 22.3858 177.614 0 150 0C122.386 0 100 22.3858 100 50C100 77.6142 122.386 100 150 100ZM50 200C77.6142 200 100 177.614 100 150C100 122.386 77.6142 100 50 100C22.3858 100 0 122.386 0 150C0 177.614 22.3858 200 50 200Z" fill="rgb(114, 114, 126)"></path>
+        <path fillRule="evenodd" clipRule="evenodd" d="M50 100C77.6142 100 100 77.6142 100 50C100 22.3858 77.6142 0 50 0C22.3858 0 0 22.3858 0 50C0 77.6142 22.3858 100 50 100ZM150 200C177.614 200 200 177.614 200 150C200 122.386 177.614 100 150 100C122.386 100 100 122.386 100 150C100 177.614 122.386 200 150 200Z" fill="rgb(98, 95, 99)"></path>
+    </svg>,
+    <svg viewBox="0 0 200 200" fill="none" >
+        <rect width="100" height="100" fill="rgb(98, 95, 99)"></rect>
+        <rect x="100" y="100" width="100" height="100" fill="rgb(114, 114, 126)"></rect>
+    </svg>,
+    <svg viewBox="0 0 200 200" fill="none" >
+        <path fillRule="evenodd" clipRule="evenodd" d="M200 200V0C89.543 0 0 89.5431 0 200H200Z" fill="rgb(98, 95, 99)"></path>
+    </svg>
+
+]
